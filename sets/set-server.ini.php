6cadfd292e9a87c384919dbb28d61b5f1a87ff7c
totoclectic <totoclectic@gmail.com>
2016-10-05 11:35:32 +0200

    create and use multiple links sets

---
+++ sets/set-server.ini.php
@@ -0,0 +1,63 @@
+; <?php die("Access denied"); ?>
+[general]
+title = "Server: Muximux - Application Management Console"
+set = "server"
+branch = "master"
+enabledropdown = "true"
+
+[settings]
+enabled = "true"
+default = "false"
+name = "Settings"
+url = "muximux.php"
+landingpage = "false"
+icon = "fa-cog"
+dd = "true"
+
+[RuTorrent]
+name = "RuTorrent"
+url = "https://github.com/Novik/ruTorrent"
+desc = "This is a little description for RuTorrent"
+icon = "fa-globe"
+enabled = "true"
+default = "true"
+landingpage = "true"
+newtab = "false"
+dd = "false"
+scale = 1
+
+[nzbhydra]
+name = "NZB Hydra"
+url = "https://github.com/theotherp/nzbhydra"
+desc = "This is a little description for NZB Hydra"
+icon = "fa-search"
+enabled = "true"
+default = "false"
+landingpage = "false"
+newtab = "false"
+dd = "false"
+scale = 1
+
+[Plex]
+name = "Plex"
+url = "https://www.plex.tv/"
+desc = "This is a little description for Plex"
+icon = "fa-play-circle"
+enabled = "true"
+default = "false"
+landingpage = "false"
+newtab = "false"
+dd = "false"
+scale = 1
+
+[deluge]
+name = "Deluge"
+url = "http://deluge-torrent.org/"
+desc = "This is a little description for Deluge"
+icon = "fa-tint"
+enabled = "true"
+default = "false"
+landingpage = "false"
+newtab = "false"
+dd = "false"
+scale = 1
